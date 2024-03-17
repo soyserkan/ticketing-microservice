6f82c3ff1316e3f1545a3702dc64023bcbfff475
soyserkan <serkansoy94@gmail.com>
2024-03-17 17:41:09 +0300

    payment module added to app module

--- order-service/src/app.module.ts
+++ order-service/src/app.module.ts
@@ -5,8 +5,9 @@ import { Config } from './config/app.config';
 import { OrderModule } from './order/order.module';
 import { TicketModule } from './ticket/ticket.module';
 import { ExpirationModule } from './expiration/expiration.module';
+import { PaymentModule } from './payment/payment.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), TypeOrmModule.forRoot(Config.database), OrderModule, TicketModule, ExpirationModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), TypeOrmModule.forRoot(Config.database), OrderModule, TicketModule, ExpirationModule, PaymentModule],
 })
 export class AppModule {}
