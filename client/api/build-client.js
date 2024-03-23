695765d099dc675ef1766f70a42b7b8c725b9181
soyserkan <serkansoy94@gmail.com>
2024-03-23 12:56:41 +0300

    delete unnecessary files

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -1,18 +1,10 @@
 import axios from 'axios';
 
 export default ({ req }) => {
-  if (typeof window === 'undefined') {
-    // We are on the server
-
-    return axios.create({
-      baseURL:
-        'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
-      headers: req.headers,
-    });
-  } else {
-    // We must be on the browser
-    return axios.create({
-      baseUrl: '/',
-    });
-  }
+  const baseURL = (typeof window === 'undefined') ? 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local' : '/';
+  const api = axios.create({ baseURL, headers: req?.headers });
+  api.interceptors.response.use((response) => response, (error) => {
+    throw error.response.data;
+  });
+  return api;
 };
